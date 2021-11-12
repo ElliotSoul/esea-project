4f25167488402319b5ec6083aa19232e8467b309
Elliot Soul <ees@valeviews.com>
2021-11-12 10:44:38 +0000

    Please

--- FlaskProject/config.py
+++ FlaskProject/config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:
     SECRET_KEY = 'a2da48312d87190b97db8652f2891b74'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///project.db'
+    SQLALCHEMY_DATABASE_URI = 'postgres://ylopxqibqhgttg:2a1c1a2777af1fe1b7548658d5bf68bea504513ede66693efed108647f3466ef@ec2-176-34-168-83.eu-west-1.compute.amazonaws.com:5432/ddpgtaobf1kfml'
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
