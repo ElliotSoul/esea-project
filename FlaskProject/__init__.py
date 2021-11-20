b6bfc2d3a29618ab78931c28351d4b816039a583
Elliot Soul <ees@valeviews.com>
2021-11-20 11:26:25 +0000

    s3

--- FlaskProject/__init__.py
+++ FlaskProject/__init__.py
@@ -4,12 +4,14 @@ from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
 from FlaskProject.config import Config
+from flask_s3 import FlaskS3
 
 db=SQLAlchemy()
 bcrypt=Bcrypt()
 login_manager=LoginManager()
 login_manager.login_view = 'users.login'
 login_manager.login_message_category = 'info'
+s3 = FlaskS3()
 
 mail = Mail()
 
@@ -20,8 +22,10 @@ def create_app(config_class=Config):
     bcrypt.init_app(app)
     login_manager.init_app(app)
     mail.init_app(app)
-    with app.app_context():
-        db.create_all()
+    #with app.app_context():
+    #    db.create_all()
+    s3.init_app(app)
+    app.config['FLASKS3_BUCKET_NAME'] = 'eseaproject'
 
     from FlaskProject.users.routes import users
     from FlaskProject.adverts.routes import adverts
@@ -33,3 +37,4 @@ def create_app(config_class=Config):
     app.register_blueprint(errors)
 
     return app
+create_app()
