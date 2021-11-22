4f289ff26c28245ae7d56275289a6f72927eaabf
Elliot Soul <ees@valeviews.com>
2021-11-22 11:46:59 +0000

    maybe?

--- FlaskProject/users/utils.py
+++ FlaskProject/users/utils.py
@@ -20,8 +20,8 @@ def save_picture(form_picture):
     with Image.open(form_picture) as img:
         img.thumbnail(final_size)
         img.save(picture_path)
-        s3 = boto3.resource('s3')
-        s3.meta.client.upload_file(picture_path, Config.S3_BUCKET, 'static/profile_pics/'+str(picture_filename))
+        content_type = request.mimetype
+        s3.put_object(Body=form_picture, Bucket=Config.S3_BUCKET, Key='static/profile_pics/'+picture_filename, ContentType=content_type)
         prev_picture = os.path.join(current_app.root_path, 'static/profile_pics', current_user.image_file)
         if os.path.exists(prev_picture) and os.path.basename(prev_picture) != 'default.jpg':
             os.remove(prev_picture) 
