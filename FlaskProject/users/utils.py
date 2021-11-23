67a46b9bada6929f7cf451e77ba8d20c18dbe8ca
Elliot Soul <ees@valeviews.com>
2021-11-23 14:55:47 +0000

    yes

--- FlaskProject/users/utils.py
+++ FlaskProject/users/utils.py
@@ -34,7 +34,7 @@ def save_ad_picture(form_picture):
     random_filename = secrets.token_hex(8)
     _, f_ext = os.path.splitext(form_picture.filename)
     picture_filename=random_filename+f_ext
-    picture_path = os.path.join(current_app.root_path, 'https://eseaproject.s3.amazonaws.com/static/advert_pics', picture_filename)
+    picture_path = 'static/advert_pics/'+ picture_filename
     final_size=(250, 250)
     with Image.open(form_picture) as img:
         img.thumbnail(final_size)
