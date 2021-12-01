f6bd351215602e8db384ce40f8363d9ff82f2f32
Elliot Soul <ees@valeviews.com>
2021-12-01 11:14:05 +0000

    testing sources

--- FlaskProject/main/routes.py
+++ FlaskProject/main/routes.py
@@ -10,8 +10,9 @@ def home():
     page = request.args.get('page', 1, type=int)
     adverts=Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)
     form=HomeFilter()
-    test="https://eseaproject.s3.eu-west-2.amazonaws.com/static/profile_pics/"
-    return render_template("home.html", adverts=adverts, form=form, homepage=True, test=test)
+    bucket_url_pfp="https://eseaproject.s3.eu-west-2.amazonaws.com/static/profile_pics/"
+    bucket_url_ad="https://eseaproject.s3.eu-west-2.amazonaws.com/static/advert_pics/"
+    return render_template("home.html", adverts=adverts, form=form, homepage=True, bucket_url_pfp=bucket_url_pfp, bucket_url_ad=bucket_url_ad)
 
 @main.route("/about")
 def about():
