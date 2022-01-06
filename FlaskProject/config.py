76fa9b05040b9dd7bb2985ca09487876ea082294
Elliot Soul <ees@valeviews.com>
2022-01-06 14:21:36 +0000

    Final Commit

--- FlaskProject/config.py
+++ FlaskProject/config.py
@@ -2,10 +2,10 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = 'postgresql://ylopxqibqhgttg:2a1c1a2777af1fe1b7548658d5bf68bea504513ede66693efed108647f3466ef@ec2-176-34-168-83.eu-west-1.compute.amazonaws.com:5432/ddpgtaobf1kfml'
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
-    MAIL_USE_TLS = True
+    MAIL_USE_TLS = False
+    MAIL_USE_SSL = True
     MAIL_USERNAME = os.environ.get('EMAIL_USER')
     MAIL_PASSWORD = os.environ.get('EMAIL_PASS')
     S3_BUCKET = os.environ.get('S3_BUCKET')
